c6adb5a5fa4f570e424da46c23ede66ba2953b3d
DESKTOP-GMQ08DS\maxime <maxime97duchateau@gmail.com>
2020-08-28 16:13:16 +0200

    try to find mistake

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,21 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Point point1 = new Point(2, new double[]{10, 10});
-        // Point point2 = new Point(2, new double[]{25, 25});
+
+        Point point1 = new Point(2, new double[]{1, 1});
+        Point point2 = new Point(2, new double[]{1, 1});
         Point point3 = new Point(2, new double[]{50, 50});
+        Point point4 = new Point(2, new double[]{51, 51});
+        Point point5 = new Point(2, new double[]{52, 52});
 
         ArrayList<Point> points = new ArrayList<>();
         points.add(point1);
-        // points.add(point2);
+        points.add(point2);
         points.add(point3);
+        points.add(point4);
+        points.add(point5);
+
+
 
         KMeans kmeans = new KMeans(2,2, points);
         kmeans.start();
